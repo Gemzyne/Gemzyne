508185b15d00742c6e3af6ebbfb719dec9091eda
Tharushi Karunarathne <tharushin30@icloud.com>
2025-08-11 15:23:52 +0530

    Update home.js

--- frontend/src/Components/Home/home.js
+++ frontend/src/Components/Home/home.js
@@ -3,15 +3,10 @@ import React from "react";
 function Home() { // <- Capital 'H'
   return (
     <div>
-      <h1>Welcome to Gemzyne</h1>
-      <h1 style={{ color: "red" }}>
-        UMTN Karunarathne meeka git hub daala anith ayawa repo ekat add Kara
-        gnna
-      </h1>
-      <p>
-        Oni Ekak den kra ganna Menna iwarai hadala Database connection Ekak meka
-        thiyenne Frontend eke src eke app.js wala
-      </p>
+<h1>
+    Gemzyne
+    </h1>
+
     </div>
   );
 }
